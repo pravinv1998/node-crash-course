20c38bf7be4e95016a2eff1151837bbb3891c17d
pravinv1998 <pravin.wargad1998@gmail.com>
2021-05-26 07:49:47 +0530

    may 26_21

--- module.js
+++ module.js
@@ -5,8 +5,71 @@
 
 // local modules
 
-const { register, login } = require("./auth");
-// import { register, login} from "./auth";
+// const { register, login } = require("./auth");
+// // import { register, login} from "./auth";
 
-register("pravinvargad");
-login("pravinv", "1998");
+// register("pravinvargad");
+// login("pravinv", "1998");
+
+const path = require("path");
+// dirname
+// console.log(path.dirname(__filename));
+// // file name
+// console.log(path.basename(__filename));
+// // extension of file
+// console.log(path.extname(__filename));
+// // parse method
+// console.log(path.parse(__filename));
+// // join method
+// console.log(path.join(__filname, "order", "main.js"));
+
+const fs = require("fs");
+
+//create file
+// fs.mkdir(path.join(__dirname, "test"), (err, _) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("file created");
+// });
+
+// write file
+
+// fs.writeFile(
+//   path.join(__dirname, "test", "test.txt"),
+//   "this is sample text\n",
+//   (err) => {
+//     if (err) {
+//       throw err;
+//     }
+
+//     fs.appendFile(
+//       path.join(__dirname, "test", "test.txt"),
+//       "more data to add",
+//       (err) => {
+//         if (err) {
+//           throw err;
+//         }
+
+//         console.log("more data added");
+//       }
+//     );
+//     console.log("file created");
+//   }
+// );
+
+// read file
+
+// fs.readFile(path.join(__dirname, "test", "test.txt"), "utf-8", (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+
+//   //   console.log(data.toString());
+//   console.log(data);
+// });
+
+// os module
+
+const os = require("os");
+console.log(os.type());
